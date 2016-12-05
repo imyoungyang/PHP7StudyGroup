1de62019906cecf181b2d2103cd73964570838e8
youngbe <im.young.yang@gmail.com>
2016-12-05 16:53:48 +0800

    move hello world to src folder

---
+++ src/HelloWorld.php
@@ -0,0 +1,16 @@
+<?php
+require __DIR__ . '/../vendor/autoload.php';
+
+// name space alias
+use StudyGroup\LanguageFeatures\Say as Say;
+
+// the same as use StudyGroup\LanguageFeatures\Say\SayHi as SayHi;
+use StudyGroup\LanguageFeatures\Say\SayHi;
+use StudyGroup\LanguageFeatures\Say\SayHello;
+
+$sayHi = new SayHi;
+$sayHello = new SayHello;
+$sayWorld = new Say\SayWorld;
+echo $sayHi->sayHi() . "\n";
+echo $sayHello->sayHello() . "\n";
+echo $sayWorld->sayWorld() . "\n";
